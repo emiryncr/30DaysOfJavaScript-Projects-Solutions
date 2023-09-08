ea337dd3294c2a815fcc4a8e95b49b2c928d19f5
Emir Yeniçeri <emiryncr@gmail.com>
2023-09-08 22:20:35 +0300

    done

--- 25_Project_World_Countries_Data_1/script.js
+++ 25_Project_World_Countries_Data_1/script.js
@@ -25,11 +25,32 @@ const sortedLanguages = Object.entries(languageCount)
   langBtn.addEventListener('click', () => {
     graphTitle.textContent = `${sortedLanguages.length} Most spoken languages in the world`
     graphWrapper.innerHTML = ""
+    const maxCount = sortedLanguages[0][1];
     for (const [language, count] of sortedLanguages) {
-        const bar = document.createElement('div');
-        bar.classList.add('bar');
-        bar.textContent = `${language} ${count.toLocaleString()}`
-        graphWrapper.appendChild(bar);
+
+      const percentage = (count / maxCount) * 100;
+      
+      const itemContainer = document.createElement('div');
+      const itemNameDiv = document.createElement('div');
+      const itemBarDiv = document.createElement('div');
+      const progressDiv = document.createElement('div');
+      const itemValueDiv = document.createElement('div');
+
+      itemContainer.className = 'itemCont'
+        itemNameDiv.className = 'itemName'
+        itemBarDiv.className = 'itemBar'
+        itemValueDiv.className = 'itemValue'
+        progressDiv.className = 'progress'
+
+        progressDiv.style.width = `${percentage}%`
+
+        itemNameDiv.textContent = `${language}`
+        itemValueDiv.textContent = `${count.toLocaleString()}`
+        itemContainer.appendChild(itemNameDiv);
+        itemContainer.appendChild(itemBarDiv);
+        itemBarDiv.appendChild(progressDiv);
+        itemContainer.appendChild(itemValueDiv);
+        graphWrapper.appendChild(itemContainer)
       }
     
 })
@@ -57,11 +78,33 @@ const top10Countries = countries_data
     popBtn.addEventListener('click', () => {
         graphTitle.textContent = `${top10Countries.length} Most populated countries in the world`
         graphWrapper.innerHTML = ""
+
+        const maxPopulation = top10Countries[0].population;
         for (const country of top10Countries) {
-            const bar = document.createElement('div');
-            bar.classList.add('bar');
-            bar.textContent = `${country.name} ${country.population.toLocaleString()}`;
-            graphWrapper.appendChild(bar);
+
+          const percentage = (country.population / maxPopulation) * 100;
+
+          const itemContainer = document.createElement('div');
+          const itemNameDiv = document.createElement('div');
+          const itemBarDiv = document.createElement('div');
+          const progressDiv = document.createElement('div');
+          const itemValueDiv = document.createElement('div');
+    
+          itemContainer.className = 'itemCont'
+            itemNameDiv.className = 'itemName'
+            itemBarDiv.className = 'itemBar'
+            itemValueDiv.className = 'itemValue'
+            progressDiv.className = 'progress'
+    
+            progressDiv.style.width = `${percentage}%`
+    
+            itemNameDiv.textContent = `${country.name}`
+            itemValueDiv.textContent = `${country.population.toLocaleString()}`
+            itemContainer.appendChild(itemNameDiv);
+            itemContainer.appendChild(itemBarDiv);
+            itemBarDiv.appendChild(progressDiv);
+            itemContainer.appendChild(itemValueDiv);
+            graphWrapper.appendChild(itemContainer)
         }
     
     })
